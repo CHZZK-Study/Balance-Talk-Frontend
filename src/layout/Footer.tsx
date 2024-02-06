f2ab4bd6c9d276d37422b8359b9e8edbccf75da6
Sunghyun627 <kksscc4@gmail.com>
2024-02-06 21:14:03 +0900

    style: Footer 컴포넌트 마크업 구현

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { css } from '@emotion/css';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div
+      className={css({
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: 'auto',
+        padding: '2rem 1rem',
+      })}
+    >
+      <span
+        className={css({
+          width: '60%',
+        })}
+      >
+        취지직 team 3 Project Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+        qui officia deserunt mollit anim id est laborum.
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
