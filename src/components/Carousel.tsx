c49b35ecf1c0ac527ba0def48e5fa309c00e2a9a
jw0097 <76507017+jw0097@users.noreply.github.com>
2024-02-06 15:18:34 +0900

    feat: carousel 기능 구현

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { css } from '@emotion/css';
+import LeftArrowButton from '../assets/svg/LeftArrowButton';
+import RightArrowButton from '../assets/svg/RightArrowButton';
 
 type CarouselProps = {
   items: JSX.Element[];
 };
 
 const Carousel: React.FC<CarouselProps> = ({ items }) => {
-  return <div>{items.map((item) => item)}</div>;
+  const totalIndex = items.length - 2;
+  const [index, setIndex] = useState(0);
+
+  const onRightArrowClickHandler = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % totalIndex);
+  };
+
+  const onLeftArrowClickHandler = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + totalIndex) % totalIndex);
+  };
+
+  return (
+    <div className={css({ display: 'flex', alignItems: 'center' })}>
+      <div>
+        <div onClick={onLeftArrowClickHandler} role="presentation">
+          <LeftArrowButton />
+        </div>
+      </div>
+      <div
+        className={css({
+          width: '1260px',
+          overflow: 'hidden',
+          position: 'relative',
+        })}
+      >
+        <div
+          className={css({
+            display: 'flex',
+            transform: `translateX(-${index * 420}px)`,
+            transition: '0.3s ease-in-out',
+          })}
+        >
+          {items.map((item) => {
+            return <div className={css({ margin: '10px' })}>{item}</div>;
+          })}
+        </div>
+      </div>
+      <div>
+        <div onClick={onRightArrowClickHandler} role="presentation">
+          <RightArrowButton />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -47,6 +47,7 @@ const LandingPage = () => {
   const CarouselItems = postInfos.map((postInfo) => {
     return <PostImage images={postInfo.balanceOptions} size="medium" />;
   });
+
   return (
     <div
       className={css({
@@ -56,7 +57,52 @@ const LandingPage = () => {
         alignItems: 'center',
       })}
     >
+      <div
+        className={css({
+          display: 'flex',
+          justifyContent: 'flex-start',
+          width: '1080px',
+          marginBottom: '20px',
+        })}
+      >
+        <h2
+          className={css({
+            fontSize: '24px',
+            fontFamily: 'SpoqaHanSansNeo-Medium',
+          })}
+        >
+          추천 게시글
+        </h2>
+      </div>
       <MainPost />
+      <div
+        className={css({
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'baseline',
+          width: '1200px',
+          marginBottom: '20px',
+          marginTop: '50px',
+        })}
+      >
+        <h2
+          className={css({
+            fontSize: '24px',
+            fontFamily: 'SpoqaHanSansNeo-Medium',
+          })}
+        >
+          더 많은 게시글
+        </h2>
+        <span
+          className={css({
+            fontFamily: 'SpoqaHanSansNeo-Regular',
+            fontSize: '14px',
+            cursor: 'pointer',
+          })}
+        >
+          더보기
+        </span>
+      </div>
       <Carousel items={CarouselItems} />
     </div>
   );
