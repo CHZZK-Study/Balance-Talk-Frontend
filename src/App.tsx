a78ae867a799a9ebd96b2ef18fc54fa978f3482e
hsgh085 <hsyu0917@gmail.com>
2024-02-13 19:08:49 +0900

    refactor: layout 파일 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Outlet, Route, Routes } from 'react-router-dom';
-import { css } from '@emotion/react';
-import Header from './layout/Header';
-import SearchBar from './layout/SearchBar';
-// import Footer from './layout/Footer';
+import { Route, Routes } from 'react-router-dom';
+import { PATH } from './constants/path';
+import { Layout, LayoutNoSearch } from './layout/Layout';
 import CreatePostPage from './pages/CreatePostPage/CreatePostPage';
 import FindPasswordPage from './pages/FindPasswordPage/FindPasswordPage';
 import LandingPage from './pages/LandingPage/LandingPage';
@@ -11,39 +9,6 @@ import LoginPage from './pages/LoginPage/LoginPage';
 import PostList from './pages/PostListPage/PostListPage';
 import PostPage from './pages/PostPage/PostPage';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
-import { PATH } from './constants/path';
-
-const Layout = () => {
-  return (
-    <>
-      <Header />
-      <SearchBar />
-      <main>
-        <Outlet />
-      </main>
-      {/* <Footer /> */}
-    </>
-  );
-};
-
-const LayoutNoSearch = () => {
-  return (
-    <>
-      <Header />
-      <main
-        css={css({
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-        })}
-      >
-        <Outlet />
-      </main>
-      {/* <Footer /> */}
-    </>
-  );
-};
 
 const App: React.FC = () => {
   return (
