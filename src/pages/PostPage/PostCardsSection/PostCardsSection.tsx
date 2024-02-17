2fd51ca9d61b60b1ce979d298965df912bda1ca1
Sunghyun627 <kksscc4@gmail.com>
2024-02-17 12:05:00 +0900

    refactor: Post의 선택지를 보여주는 부분을 PostCardsSection으로 분리

---
+++ src/pages/PostPage/PostCardsSection/PostCardsSection.tsx
@@ -0,0 +1,101 @@
+import { css } from '@emotion/react';
+import React from 'react';
+import TagButton from '../../../components/TagButton';
+import Eye from '../../../assets/svg/Eye';
+import Heart from '../../../assets/svg/Heart';
+import PostOptionCard from '../../../components/PostOptionCard';
+
+const PostCardsSection = () => {
+  return (
+    <>
+      <div
+        css={css({
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: '0.5rem 1rem',
+        })}
+      >
+        <div
+          css={css({
+            display: 'flex',
+            padding: '0.5rem',
+            gap: '2rem',
+          })}
+        >
+          <div
+            css={css({
+              fontFamily: 'SpoqaHanSansNeo-medium',
+              fontSize: '3rem',
+            })}
+          >
+            카페 주문
+          </div>
+          <div
+            css={css({
+              display: 'flex',
+              alignItems: 'center',
+              gap: '0.5rem',
+            })}
+          >
+            <TagButton tag="태그" />
+            <TagButton tag="태그" />
+          </div>
+        </div>
+        <div
+          css={css({
+            display: 'flex',
+            alignItems: 'center',
+            gap: '1rem',
+            fontFamily: 'SpoqaHanSansNeo-Regular',
+            fontSize: '1.5rem',
+          })}
+        >
+          <div
+            css={css({
+              display: 'flex',
+              alignItems: 'center',
+              gap: '0.5rem',
+            })}
+          >
+            <Eye />
+            <span>100</span>
+          </div>
+          <div
+            css={css({
+              display: 'flex',
+              alignItems: 'center',
+              gap: '0.5rem',
+            })}
+          >
+            <Heart />
+            100
+          </div>
+        </div>
+      </div>
+
+      <div
+        css={css({
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '1rem',
+          backgroundColor: '#EEEEEE',
+        })}
+      >
+        <PostOptionCard />
+        <div
+          css={css({
+            fontFamily: 'SpoqaHanSansNeo-medium',
+            fontSize: '3rem',
+          })}
+        >
+          VS
+        </div>
+        <PostOptionCard />
+      </div>
+    </>
+  );
+};
+
+export default PostCardsSection;
