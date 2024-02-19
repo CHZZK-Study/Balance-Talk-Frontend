a0b6d6c86c35cf4f6f0e67cdef6417dee63e7d3c
jw0097 <76507017+jw0097@users.noreply.github.com>
2024-02-19 16:40:07 +0900

    chore: 환경 변수 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,83 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-module.exports = {
-  name: 'example',
-  mode: 'development',
-  entry: './src/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            '@babel/preset-env',
-            [
-              '@babel/preset-react',
-              { runtime: 'automatic', importSource: '@emotion/react' },
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+
+module.exports = (env) => {
+  const { DEV } = env;
+
+  if (DEV) {
+    dotenv.config({ path: './.env.local' });
+  } else {
+    dotenv.config({ path: './.env.production' });
+  }
+
+  return {
+    name: 'example',
+    mode: 'development',
+    entry: './src/index.tsx',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?/,
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              [
+                '@babel/preset-react',
+                { runtime: 'automatic', importSource: '@emotion/react' },
+              ],
+              '@babel/preset-typescript',
             ],
-            '@babel/preset-typescript',
-          ],
-          plugins: [
-            '@emotion/babel-plugin', // 추가
-          ],
-        },
-      },
-      {
-        test: /\.(css|scss)$/i,
-        use: ['style-loader', 'css-loader'],
-        include: [path.resolve(__dirname, 'src/styles')],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-            },
+            plugins: [
+              '@emotion/babel-plugin', // 추가
+            ],
           },
-        ],
-      },
-      {
-        test: /\.svg$/,
-        use: {
-          loader: '@svgr/webpack',
         },
-      },
+        {
+          test: /\.(css|scss)$/i,
+          use: ['style-loader', 'css-loader'],
+          include: [path.resolve(__dirname, 'src/styles')],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.svg$/,
+          use: {
+            loader: '@svgr/webpack',
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new webpack.EnvironmentPlugin(['API_URL']),
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  output: {
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  devServer: {
-    host: 'localhost',
-    port: 3000,
-    open: true,
-    historyApiFallback: true,
-  },
+    output: {
+      filename: 'bundle.js',
+      publicPath: '/',
+    },
+    devServer: {
+      host: 'localhost',
+      port: 3000,
+      open: true,
+      historyApiFallback: true,
+    },
+  };
 };
