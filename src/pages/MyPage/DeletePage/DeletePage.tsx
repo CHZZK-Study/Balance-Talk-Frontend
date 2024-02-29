637302b6cafd962f02c22c53f7c40731f8e67290
hsgh085 <hsyu0917@gmail.com>
2024-03-01 03:08:43 +0900

    refactor: 회원탈퇴 경고문구 컴포넌트로 분리

--- src/pages/MyPage/DeletePage/DeletePage.tsx
+++ src/pages/MyPage/DeletePage/DeletePage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { css } from '@emotion/react';
 import { UseQueryResult, useQuery } from '@tanstack/react-query';
 import { axiosInstance } from '../../../api/interceptor';
-import Caution from '../../../assets/svg/Caution';
 import Heading from '../../../components/design/Heading/Heading';
 import { END_POINT } from '../../../constants/api';
-import { Theme } from '../../../styles/Theme';
 import { Member } from '../../../types/mypage';
 import { loginContainer } from '../../LoginPage/LoginPage.style';
 import DeleteForm from './sections/DeleteForm/DeleteForm';
+import WarningMessage from './sections/WarningMessage/WarningMessage';
 
 const DeletePage = () => {
   const { data: member }: UseQueryResult<Member, Error> = useQuery({
@@ -27,21 +25,7 @@ const DeletePage = () => {
     <div css={loginContainer}>
       {member && <span>{member.nickname}</span>}
       <Heading size="medium">회원 탈퇴</Heading>
-      <div css={css({ display: 'flex', alignItems: 'center', gap: '10px' })}>
-        <Caution />
-        <span
-          css={css({
-            color: Theme.color.red,
-            fontSize: Theme.text.xSmall.fontSize,
-            lineHeight: Theme.text.xSmall.lineHeight,
-            fontWeight: 600,
-          })}
-        >
-          탈퇴 후 5일간 재가입이 불가능합니다.
-          <br /> 탈퇴한 경우 게시글 및 댓글 삭제가 불가능합니다.
-        </span>
-      </div>
-
+      <WarningMessage />
       <DeleteForm />
     </div>
   );
---
+++ src/pages/MyPage/DeletePage/sections/WarningMessage/WarningMessage.style.ts
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react';
+import { Theme } from '../../../../../styles/Theme';
+
+export const warningMessageContainer = css({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+});
+
+export const waringMessageTextStyling = css({
+  color: Theme.color.red,
+  fontSize: Theme.text.xSmall.fontSize,
+  lineHeight: Theme.text.xSmall.lineHeight,
+  fontWeight: 600,
+});
