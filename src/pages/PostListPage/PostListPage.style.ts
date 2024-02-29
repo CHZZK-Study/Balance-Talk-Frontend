95287d3c281acb7ede9cb66eabbdcb286c300667
jw0097 <76507017+jw0097@users.noreply.github.com>
2024-02-29 17:44:06 +0900

    refactor: 게시물 페이지 css 및 컴포넌트 리팩토링

---
+++ src/pages/PostListPage/PostListPage.style.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+
+export const headingWrapper = css({
+  display: 'flex',
+  justifyContent: 'space-between',
+  width: '80%',
+  margin: '0 auto',
+  alignItems: 'center',
+});
+
+export const postListWrapper = css({
+  display: 'flex',
+  width: '90%',
+  justifyContent: 'space-between',
+  flexWrap: 'wrap',
+  margin: '0 auto',
+});
