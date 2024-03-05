7ccfb1476255e32e2fe4c07c09d48685d2609c99
Sunghyun627 <kksscc4@gmail.com>
2024-03-05 20:00:02 +0900

    refactor: BalancOptionCardsSection으로 부터 TitleSection 분리 및 스타일 코드 분리

---
+++ src/pages/PostPage/BalanceOptionCardsSection/BalanceOptionCardsSection.style.ts
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react';
+
+export const balanceOptionCardsSectionWrapper = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '1rem',
+  backgroundColor: '#EEEEEE',
+});
+
+export const versusTextwrapper = css({
+  fontFamily: 'SpoqaHanSansNeo-Bold',
+  fontSize: '50px',
+  position: 'absolute',
+  zIndex: 1,
+  WebkitTextStroke: '1px white',
+  textShadow: '0px 4px 4px gray',
+});
