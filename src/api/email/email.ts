8086ade6f7882ef49e631c07107f395d648628c7
hsgh085 <hsyu0917@gmail.com>
2024-03-17 03:58:22 +0900

    feat: 인증번호 검증 api 연결

--- src/api/email/email.ts
+++ src/api/email/email.ts
@@ -1,11 +1,16 @@
 import { END_POINT } from '@/constants/api';
+import { MemberForm } from '@/types/member';
 import { AxiosError } from 'axios';
 import { axiosInstance } from '../interceptor';
 
-export const postEmailVerify = async () => {
-  const { data } = await axiosInstance.post<string>(
+export const postEmailVerify = async (
+  value: Pick<MemberForm, 'email' | 'code'>,
+) => {
+  const { data } = await axiosInstance.post<string | AxiosError<'response'>>(
     `${END_POINT.EMAIL_VERIFY}`,
+    value,
   );
+
   return data;
 };
 
