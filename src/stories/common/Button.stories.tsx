31d511b0a3355b1b1073d465aeb8bec08dabcd40
Yu,Hee Seon <84820008+hsgh085@users.noreply.github.com>
2024-06-17 18:26:42 +0900

    chore: Storybook 셋팅 (#114)
    * chore: Storybook 셋팅
    
    * feat: story 템플릿 예시 작성
    
    * refactor: 절대경로로 변경
    
    * feat: chromatic을 통해 storybook 배포
    
    * chore: github actions 설정

---
+++ src/stories/common/Button.stories.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import Button from '@/components/design/Button/Button';
+import type { Meta, StoryObj } from '@storybook/react';
+import { storyContainer, storyInnerContainer } from '@/stories/story.styles';
+
+const meta = {
+  title: 'Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    size: {
+      options: ['xSmall', 'small', 'medium', 'large'],
+      control: { type: 'radio' },
+    },
+    variant: {
+      options: ['default', 'outline', 'cancel'],
+      control: { type: 'radio' },
+    },
+    children: { control: { type: 'text' } },
+  },
+  args: {
+    variant: 'default',
+    size: 'medium',
+    children: 'Button',
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: 'default',
+    size: 'medium',
+  },
+};
+
+export const All: Story = {
+  render: (args) => (
+    <ul css={storyContainer}>
+      <li css={storyInnerContainer}>
+        <h3>Size</h3>
+        <Button {...args} size="xSmall" variant="default">
+          XSmall
+        </Button>
+        <Button {...args} size="small" variant="default">
+          Small
+        </Button>
+        <Button {...args} size="medium" variant="default">
+          Medium
+        </Button>
+        <Button {...args} size="large" variant="default">
+          Large
+        </Button>
+      </li>
+      <li css={storyInnerContainer}>
+        <h3>Variant</h3>
+        <Button {...args} size="medium" variant="default">
+          Default
+        </Button>
+        <Button {...args} size="medium" variant="outline">
+          Outline
+        </Button>
+        <Button {...args} size="medium" variant="cancel">
+          Cancel
+        </Button>
+      </li>
+    </ul>
+  ),
+};
---
+++ src/stories/story.styles.ts
@@ -0,0 +1,14 @@
+import { css } from '@emotion/react';
+
+export const storyContainer = css({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '20px',
+});
+
+export const storyInnerContainer = css({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px',
+  alignItems: 'flex-start',
+});
