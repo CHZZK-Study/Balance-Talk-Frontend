7280345f9babb0ece61b44a100cba858c7c203cb
hsgh085 <hsyu0917@gmail.com>
2024-06-20 17:13:41 +0900

    refactor: Label 컴포넌트 수정

--- src/components/design/Label/Label.style.ts
+++ src/components/design/Label/Label.style.ts
@@ -1,10 +1,6 @@
+import typo from '@/styles/typo';
 import { css } from '@emotion/react';
 
-import { Theme } from '../../../styles/Theme';
-
-export const labelStyling = css({
-  fontSize: Theme.text.small.fontSize,
-  lineHeight: Theme.text.small.lineHeight,
-  fontWeight: 600,
+export const labelStyling = css(typo.Body.Medium_5, {
   whiteSpace: 'nowrap',
 });
