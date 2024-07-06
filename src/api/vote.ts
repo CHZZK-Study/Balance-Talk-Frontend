19aab78c781458746e7fcf162c9b04026b32a3e2
kwaksj329 <kwaksj329@gmail.com>
2024-07-07 01:43:19 +0900

    refactor: vote API parameter 타입 수정

--- src/api/vote.ts
+++ src/api/vote.ts
@@ -1,8 +1,9 @@
 import { END_POINT } from '@/constants/api';
 import { VoteOption, VoteResult } from '@/types/vote';
+import { Id } from '@/types/api';
 import { axiosInstance } from './interceptor';
 
-export const getTalkPickVoteResult = async (talkPickId: number) => {
+export const getTalkPickVoteResult = async (talkPickId: Id) => {
   const { data } = await axiosInstance.get<VoteResult>(
     END_POINT.VOTE_TALK(talkPickId),
   );
@@ -10,7 +11,7 @@ export const getTalkPickVoteResult = async (talkPickId: number) => {
 };
 
 export const putTalkPickVoteResult = async (
-  talkPickId: number,
+  talkPickId: Id,
   option: VoteOption,
 ) => {
   const response = await axiosInstance.put(END_POINT.VOTE_TALK(talkPickId), {
@@ -20,7 +21,7 @@ export const putTalkPickVoteResult = async (
 };
 
 export const postTalkPickVoteResult = async (
-  talkPickId: number,
+  talkPickId: Id,
   option: VoteOption,
 ) => {
   const response = await axiosInstance.post(END_POINT.VOTE_TALK(talkPickId), {
@@ -29,24 +30,21 @@ export const postTalkPickVoteResult = async (
   return response;
 };
 
-export const getGameVoteResult = async (gameId: number) => {
+export const getGameVoteResult = async (gameId: Id) => {
   const { data } = await axiosInstance.get<VoteResult>(
     END_POINT.VOTE_GAME(gameId),
   );
   return data;
 };
 
-export const putGameVoteResult = async (gameId: number, option: VoteOption) => {
+export const putGameVoteResult = async (gameId: Id, option: VoteOption) => {
   const response = await axiosInstance.put(END_POINT.VOTE_GAME(gameId), {
     voteOption: option,
   });
   return response;
 };
 
-export const postGameVoteResult = async (
-  gameId: number,
-  option: VoteOption,
-) => {
+export const postGameVoteResult = async (gameId: Id, option: VoteOption) => {
   const response = await axiosInstance.post(END_POINT.VOTE_GAME(gameId), {
     voteOption: option,
   });
