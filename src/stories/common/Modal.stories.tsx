67b940f2fb101538c252d24daedf3b601a95d812
areumH <94692566+areumH@users.noreply.github.com>
2024-07-22 02:56:44 +0900

    feat: common 컴포넌트 스토리북에 추가

--- src/stories/common/Modal.stories.tsx
+++ src/stories/common/Modal.stories.tsx
@@ -1,38 +1,44 @@
 import React from 'react';
-import Chips from '@/components/common/Chips/Chips';
+import Modal from '@/components/common/Modal/Modal';
 import type { Meta, StoryObj } from '@storybook/react';
 import { storyContainer, storyInnerContainer } from '@/stories/story.styles';
 
 const meta = {
-  title: 'commons/Chips',
-  component: Chips,
+  title: 'commons/Modal',
+  component: Modal,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
   argTypes: {
+    action: {
+      options: ['default', 'share'],
+      control: { type: 'radio' },
+    },
     children: { control: { type: 'text' } },
   },
   args: {
-    children: 'CHIP',
+    children: '모달 확인용 텍스트',
   },
-} satisfies Meta<typeof Chips>;
+} satisfies Meta<typeof Modal>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    action: 'default',
+  },
+};
 
 export const All: Story = {
   render: () => (
     <ul css={storyContainer}>
       <li css={storyInnerContainer}>
-        <h3>Size</h3>
-        <Chips>Large</Chips>
-      </li>
-      <li css={storyContainer}>
-        <h3>Variant</h3>
-        <Chips>Outline</Chips>
+        <h3>Default</h3>
+        <Modal>기본 팝업 확인용 텍스트</Modal>
+        <h3>Share</h3>
+        <Modal action="share">공유하기 팝업 확인용 텍스트</Modal>
       </li>
     </ul>
   ),
