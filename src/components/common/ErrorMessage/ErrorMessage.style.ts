fbebc58cf58119522657dc30604e8bf6b274a68b
hsgh085 <hsyu0917@gmail.com>
2024-07-24 03:47:43 +0900

    feat: ErrorMessage UI style 수정

--- src/components/common/ErrorMessage/ErrorMessage.style.ts
+++ src/components/common/ErrorMessage/ErrorMessage.style.ts
@@ -6,6 +6,6 @@ import type { ErrorMessageProps } from './ErrorMessage';
 export const getTextStyling = (
   isError: Required<ErrorMessageProps>['isError'],
 ) =>
-  css(typo.Body.Medium_5, {
-    color: isError ? color.Red[500] : color.Neutral[600],
+  css(typo.Main.Medium, {
+    color: isError ? color.RED : color.GY[1],
   });
