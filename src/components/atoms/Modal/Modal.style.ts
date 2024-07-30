4ed2464ef0284fcfae0856fb6dba0827a96925bd
areumH <94692566+areumH@users.noreply.github.com>
2024-07-30 16:55:50 +0900

    design: modal 컴포넌트 스타일 수정

--- src/components/atoms/Modal/Modal.style.ts
+++ src/components/atoms/Modal/Modal.style.ts
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import color from '@/styles/color';
 import type { ModalProps } from './Modal';
 
 export const modalStyling = css({
@@ -7,15 +8,33 @@ export const modalStyling = css({
   alignItems: 'center',
   position: 'relative',
   width: '598px',
-  padding: '34px 30px',
-  backgroundColor: '#F1F1F1',
+  backgroundColor: color.GY[3],
   borderRadius: '10px',
   boxShadow: '1px 2px 30px rgba(0, 0, 0, 0.15)',
 });
 
-export const getModalPadding = (action: Required<ModalProps>['action']) => css`
-  ${action === 'share' && 'padding-bottom: 12px;'}
-`;
+export const getModalSize = (action: Required<ModalProps>['action']) => {
+  const style = {
+    default: css({
+      padding: '34px 30px',
+    }),
+    share: css({
+      paddingTop: '34px',
+      paddingLeft: '30px',
+      paddingRight: '30px',
+      paddingBottom: '12px',
+    }),
+    report: css({
+      width: '495px',
+      paddingTop: '39px',
+      paddingLeft: '33px',
+      paddingRight: '33px',
+      paddingBottom: '41px',
+    }),
+  };
+
+  return style[action as keyof typeof style];
+};
 
 export const modalCloseStyling = css({
   position: 'absolute',
