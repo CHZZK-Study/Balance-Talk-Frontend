094beb1c2e7fefc7cc30e2e1f0bb1787e3045095
onejuneee <onejuneee721@gmail.com>
2024-08-07 07:47:54 +0900

    feat: Toggle 컴포넌트 UI 구현

---
+++ src/components/atoms/Toggle/Toggle.style.ts
@@ -0,0 +1,26 @@
+import { css } from '@emotion/react';
+import color from '@/styles/color';
+import typo from '@/styles/typo';
+
+export const container = css`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 8px 16px;
+  border: 1px solid ${color.GY[2]};
+  border-radius: 4px;
+  background-color: ${color.WT};
+  gap: 8px;
+`;
+
+export const label = css`
+  ${typo.Main.Medium}
+  color: ${color.BK};
+  text-align: center;
+`;
+
+export const count = css`
+  text-align: center;
+  ${typo.Number.Medium_18}
+  color: ${color.MAIN};
+`;
