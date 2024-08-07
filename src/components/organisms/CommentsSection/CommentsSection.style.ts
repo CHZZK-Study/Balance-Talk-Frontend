2a4dfe511cd6e97a10100e03a98cacea65ffa477
onejuneee <onejuneee721@gmail.com>
2024-08-08 04:31:38 +0900

    design: 블러 관련 스타일 추가, 토스트 모달 스타일 추가

--- src/components/organisms/CommentsSection/CommentsSection.style.ts
+++ src/components/organisms/CommentsSection/CommentsSection.style.ts
@@ -4,12 +4,34 @@ import color from '@/styles/color';
 export const commentsSectionContainer = css`
   display: flex;
   flex-direction: column;
-  width: 100%;
   min-height: 1554px;
-  height: 100%;
   margin: 0 auto;
   background-color: ${color.WT};
   overflow-y: auto;
+  gap: 23px;
+`;
+
+export const loggedInBackground = css`
+  width: 100%;
+  height: auto;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 29px;
+  position: relative;
+`;
+
+export const loggedOutBackground = css`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.01);
+  backdrop-filter: blur(11px);
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
   gap: 29px;
 `;
 
@@ -18,19 +40,25 @@ export const commentsWrapper = css`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  list-style: none;
   padding: 0;
   margin: 0;
   min-height: 917px;
   height: auto;
-  background-color: aquamarine;
 `;
 
 export const paginationWrapper = css`
   display: flex;
   justify-content: center;
-  margin-top: 11px;
+  margin-top: 17px;
   width: 100%;
   height: 40px;
   flex-shrink: 0;
+`;
+
+export const toastModalWrapper = css`
+  position: absolute;
+  top: 76px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 10;
 `;
