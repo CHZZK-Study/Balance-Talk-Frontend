e1c34e6f3574694ba7dad666f382c895f568f38b
onejuneee <onejuneee721@gmail.com>
2024-08-14 03:44:28 +0900

    refactor: 객체 매핑을 통해 interaction일 경우를 우선 탐색, 그 후 이미지 아이콘을 반환하는 형태로 전환

--- src/components/atoms/ProfileIcon/ProfileIcon.tsx
+++ src/components/atoms/ProfileIcon/ProfileIcon.tsx
@@ -1,27 +1,39 @@
 import React from 'react';
-import { NormalProfile } from '@/assets';
+import { NormalProfile, LargeProfile } from '@/assets';
 import { profileWrapper, profileImage } from './ProfileIcon.style';
 
 interface ProfileProps {
   interaction: 'normal';
   imgUrl?: string;
+  size?: 'small' | 'large';
 }
 
 interface ProfilePropsWithImage {
   interaction: 'settings';
   imgUrl: string;
+  size?: 'small' | 'large';
 }
 
 const ProfileIcon = ({
   interaction = 'normal',
   imgUrl,
-}: ProfileProps | ProfilePropsWithImage) =>
-  interaction === 'normal' ? (
-    <NormalProfile />
-  ) : (
-    <div css={profileWrapper}>
-      <img css={profileImage} src={imgUrl} alt="profile" />
-    </div>
-  );
+  size = 'small',
+}: ProfileProps | ProfilePropsWithImage) => {
+  const profileComponents = {
+    normal: {
+      small: <NormalProfile />,
+      large: <LargeProfile />,
+    },
+    settings: (
+      <div css={profileWrapper(size)}>
+        <img css={profileImage} src={imgUrl} alt="profile" />
+      </div>
+    ),
+  };
+
+  return interaction === 'settings'
+    ? profileComponents.settings
+    : profileComponents.normal[size];
+};
 
 export default ProfileIcon;
