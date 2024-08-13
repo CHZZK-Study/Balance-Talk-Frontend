d9d9d678092920a19aacbbb678bf695fb9a0de40
onejuneee <onejuneee721@gmail.com>
2024-08-14 03:44:58 +0900

    feat: 수정된 profileIcon 컴포넌트 스토리북 반영

--- src/stories/atoms/ProfileIcon.stories.tsx
+++ src/stories/atoms/ProfileIcon.stories.tsx
@@ -16,9 +16,14 @@ const meta = {
       control: { type: 'radio' },
     },
     imgUrl: { control: { type: 'text' } },
+    size: {
+      options: ['small', 'large'],
+      control: { type: 'radio' },
+    },
   },
   args: {
     interaction: 'normal',
+    size: 'small',
   },
 } satisfies Meta<typeof ProfileIcon>;
 
@@ -28,6 +33,7 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     interaction: 'normal',
+    size: 'small',
   },
 };
 
@@ -35,12 +41,22 @@ export const All: Story = {
   render: (args) => (
     <ul css={storyContainer}>
       <li css={storyInnerContainer}>
-        <h3>Normal</h3>
-        <ProfileIcon {...args} />
-        <h3>Settings</h3>
+        <h3>Normal Small</h3>
+        <ProfileIcon {...args} size="small" />
+        <h3>Normal Large</h3>
+        <ProfileIcon {...args} size="large" />
+
+        <h3>Settings Small</h3>
         <ProfileIcon
           interaction="settings"
-          imgUrl="https://www.figma.com/design/NLSmVbazYeN8UxT6sB9qv6/%5BPICK-O%5D-%EC%99%80%EC%9D%B4%EC%96%B4%ED%94%84%EB%A0%88%EC%9E%84?node-id=1474-16802&t=2zl1YNC4nZTO3g09-4"
+          imgUrl="https://www.example.com/path/to/image.jpg"
+          size="small"
+        />
+        <h3>Settings Large</h3>
+        <ProfileIcon
+          interaction="settings"
+          imgUrl="https://www.example.com/path/to/image.jpg"
+          size="large"
         />
       </li>
     </ul>
