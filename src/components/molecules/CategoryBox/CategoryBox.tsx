aa65ddbba82f4ca9c9aa5e2e8bdc11cc99e7f0a6
areumH <94692566+areumH@users.noreply.github.com>
2024-08-21 00:06:46 +0900

    refactor: 톡앤픽 플레이스 링크 소문자로 수정

--- src/components/molecules/CategoryBox/CategoryBox.tsx
+++ src/components/molecules/CategoryBox/CategoryBox.tsx
@@ -6,7 +6,7 @@ import { categoryBoxStyling } from './CategoryBox.style';
 const CategoryBox = () => {
   return (
     <div css={categoryBoxStyling}>
-      <Link to="/talkPickPlace">
+      <Link to="/talkpickplace">
         <CategoryButton imageType="PickVote" label="톡&픽 플레이스" />
       </Link>
       <CategoryButton imageType="TodayPick" label="오늘의 톡픽 모음.zip" />
