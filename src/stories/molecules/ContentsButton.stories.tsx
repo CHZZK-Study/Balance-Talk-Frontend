f9f3605137a1709541eaaefd1bc5c3903d3368bf
onejuneee <onejuneee721@gmail.com>
2024-08-24 04:40:12 +0900

    refactor: 수정된 ContentsButton 컴포넌트 스토리북 추가

--- src/stories/molecules/ContentsButton.stories.tsx
+++ src/stories/molecules/ContentsButton.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { css } from '@emotion/react';
 import ContentsButton from '@/components/molecules/ContentsButton/ContentsButton';
-import { SampleWhole } from '@/assets';
+import { SampleFirst, SampleSecond } from '@/assets';
 
 const meta: Meta<typeof ContentsButton> = {
   title: 'molecules/ContentsButton',
@@ -12,16 +12,18 @@ const meta: Meta<typeof ContentsButton> = {
   },
   tags: ['autodocs'],
   argTypes: {
-    imgUrl: { control: 'text' },
+    imgUrl: { control: { type: 'object' } },
     label: { control: 'text' },
     tagLabels: { control: { type: 'object' } },
     bookmarkState: { control: 'boolean' },
+    showBookmark: { control: 'boolean' },
   },
   args: {
-    imgUrl: SampleWhole,
+    imgUrl: [SampleFirst, SampleSecond],
     label: '유진 VS 민지 사복 고르기',
     tagLabels: ['얼마나 맞나 보자', '#취향'],
     bookmarkState: false,
+    showBookmark: true,
   },
 };
 
@@ -47,10 +49,11 @@ const headerStyle = css`
 
 export const Default: Story = {
   args: {
-    imgUrl: SampleWhole,
+    imgUrl: [SampleFirst, SampleSecond],
     label: '유진 VS 민지 사복 고르기',
     tagLabels: ['얼마나 맞나 보자', '#취향'],
     bookmarkState: false,
+    showBookmark: true,
   },
 };
 
@@ -59,11 +62,15 @@ export const All: Story = {
     <div css={containerStyle}>
       <div css={itemStyle}>
         <h3 css={headerStyle}>기본</h3>
-        <ContentsButton {...args} bookmarkState={false} />
+        <ContentsButton {...args} bookmarkState={false} showBookmark />
       </div>
       <div css={itemStyle}>
         <h3 css={headerStyle}>북마크 pressed</h3>
-        <ContentsButton {...args} bookmarkState />
+        <ContentsButton {...args} bookmarkState showBookmark />
+      </div>
+      <div css={itemStyle}>
+        <h3 css={headerStyle}>북마크 없음</h3>
+        <ContentsButton {...args} showBookmark={false} />
       </div>
     </div>
   ),
