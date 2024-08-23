c76e82bf379a5df2e82b3711438b196e7f78a54c
onejuneee <onejuneee721@gmail.com>
2024-08-24 07:30:26 +0900

    refactor: 버튼 클릭 핸들러를 ActionBox에서 처리하도록 수정

--- src/components/molecules/ActionBox/ActionBox.tsx
+++ src/components/molecules/ActionBox/ActionBox.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ActionButton from '@/components/atoms/ActionButton/ActionButton';
 import { actionBoxContainer } from './ActionBox.style';
 
 const ActionBox = () => {
+  const [activeButton, setActiveButton] = useState('activity');
+
+  const handleButtonClick = (buttonType: string) => {
+    setActiveButton(buttonType);
+  };
+
   return (
     <div css={actionBoxContainer}>
-      <ActionButton label="활동 내역" iconType="activity" />
-      <ActionButton label="회원정보 수정" iconType="edit" />
-      <ActionButton label="회원탈퇴" iconType="withdraw" />
+      <ActionButton
+        label="활동 내역"
+        iconType="activity"
+        isActive={activeButton === 'activity'}
+        onClick={() => handleButtonClick('activity')}
+      />
+      <ActionButton
+        label="회원정보 수정"
+        iconType="edit"
+        isActive={activeButton === 'edit'}
+        onClick={() => handleButtonClick('edit')}
+      />
+      <ActionButton
+        label="회원탈퇴"
+        iconType="withdraw"
+        isActive={activeButton === 'withdraw'}
+        onClick={() => handleButtonClick('withdraw')}
+      />
     </div>
   );
 };
