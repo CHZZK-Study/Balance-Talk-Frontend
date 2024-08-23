4330288138002b28c46d4c603e00f052f74ea1dc
onejuneee <onejuneee721@gmail.com>
2024-08-24 04:51:57 +0900

    feat: OptionSelector molecules 컴포넌트 UI 구현

---
+++ src/components/molecules/OptionSelector/OptionSelector.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Button from '@/components/atoms/Button/Button';
+import * as S from './OptionSelector.style';
+
+export interface OptionSelectorProps {
+  options: string[];
+  selectedOption?: string;
+  onSelect: (option: string) => void;
+}
+
+const OptionSelector = ({
+  options,
+  selectedOption,
+  onSelect,
+}: OptionSelectorProps) => {
+  const [selected, setSelected] = useState(selectedOption || options[0]);
+
+  const handleSelect = (option: string) => {
+    setSelected(option);
+    onSelect(option);
+  };
+
+  return (
+    <div css={S.optionSelectorContainer}>
+      {options.map((option) => (
+        <Button
+          key={option}
+          variant={selected === option ? 'roundPrimary2' : 'outlinePrimary'}
+          size="large"
+          onClick={() => handleSelect(option)}
+        >
+          {option}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default OptionSelector;
