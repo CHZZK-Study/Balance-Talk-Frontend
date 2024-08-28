d721428b2c3969a5cc8c84c2468ac2ebc95ea07e
onejuneee <onejuneee721@gmail.com>
2024-08-28 09:20:40 +0900

    feat: MyBalanceGameList organisms 컴포넌트 UI 구현

---
+++ src/components/organisms/MyBalanceGameList/MyBalanceGameList.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ContentsButton from '@/components/molecules/ContentsButton/ContentsButton';
+import { MyBalanceGameListProps, MyBalanceGameItem } from '@/types/organisms';
+import * as S from './MyBalanceGameList.style';
+
+const MyBalanceGameList = ({ items }: MyBalanceGameListProps) => {
+  const groupedItems = items.reduce(
+    (acc, item) => {
+      if (!acc[item.editedAt]) {
+        acc[item.editedAt] = [];
+      }
+      acc[item.editedAt].push(item);
+      return acc;
+    },
+    {} as Record<string, MyBalanceGameItem[]>,
+  );
+
+  return (
+    <div css={S.container}>
+      {Object.keys(groupedItems).map((date) => (
+        <div key={date} css={S.dateWrapper}>
+          <span css={S.dateLabel}>{date}</span>
+          <ul css={S.contentList}>
+            {groupedItems[date].map((balanceGameItem) => (
+              <li key={balanceGameItem.id} css={S.contentItem}>
+                <ContentsButton
+                  optionAImg={balanceGameItem.optionAImg}
+                  optionBImg={balanceGameItem.optionBImg}
+                  title={balanceGameItem.title}
+                  tagLabels={balanceGameItem.tagLabels}
+                  bookmarkState={balanceGameItem.bookmarkState}
+                  showBookmark={balanceGameItem.showBookmark}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MyBalanceGameList;
