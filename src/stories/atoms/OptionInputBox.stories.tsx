386d40783408677cb90c43b421ef19a2770da972
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-08-28 12:23:02 +0900

    refactor: storybook OptionInputBox 컴포넌트 수정

--- src/stories/atoms/OptionInputBox.stories.tsx
+++ src/stories/atoms/OptionInputBox.stories.tsx
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import store from '@/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ReactQueryProvider from '@/providers/ReactQueryProvider';
 import type { Meta, StoryObj } from '@storybook/react';
 import OptionInputBox from '@/components/atoms/OptionInputBox/OptionInputBox';
 
@@ -13,10 +19,29 @@ const meta = {
   },
   args: {
     option: 'A',
+    value: '',
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => {},
   },
+  decorators: [
+    (Story) => (
+      <Provider store={store}>
+        <ReactQueryProvider>
+          <Router>
+            <Story />
+          </Router>
+        </ReactQueryProvider>
+      </Provider>
+    ),
+  ],
 } satisfies Meta<typeof OptionInputBox>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    option: 'A',
+    value: '',
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => {},
+  },
+};
