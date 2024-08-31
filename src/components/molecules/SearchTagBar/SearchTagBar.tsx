b55488c5008b19ab13f05d4a269e0ce0ea7c6548
areumH <94692566+areumH@users.noreply.github.com>
2024-09-01 02:53:19 +0900

    refactor: 키워드 태그 constants 폴더로 분리

--- src/components/molecules/SearchTagBar/SearchTagBar.tsx
+++ src/components/molecules/SearchTagBar/SearchTagBar.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import Button from '@/components/atoms/Button/Button';
 import SearchBar from '@/components/atoms/SearchBar/SearchBar';
+import { KEYWORD_TAG } from '@/constants/keywordTag';
 import * as S from './SearchTagBar.style';
 
 const SearchTagBar = () => {
-  const keyWordArray: string[] = [
-    '이상형 월드컵',
-    '인기',
-    '커플',
-    '개그',
-    '음식',
-    '여행',
-    '영화 월드컵',
-  ];
-
   return (
     <div css={S.searchBarContainerStyling}>
       <SearchBar />
       <div css={S.btnWrapperStyling}>
-        {keyWordArray.map((keyWord) => (
+        {KEYWORD_TAG.map((keyWord) => (
           <Button key={keyWord} size="large" variant="outlinePrimary">
             #{keyWord}
           </Button>
