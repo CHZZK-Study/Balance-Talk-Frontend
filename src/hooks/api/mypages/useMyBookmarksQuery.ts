8a0b0e4465f30adf4845eb2748bd765962071410
onejuneee <onejuneee721@gmail.com>
2024-09-02 06:49:24 +0900

    refactor: 데이터 가공 부분과 infinity-query 로직 분리(관심사 분리)

--- src/hooks/api/mypages/useMyBookmarksQuery.ts
+++ src/hooks/api/mypages/useMyBookmarksQuery.ts
@@ -1,44 +1,35 @@
-import { useInfiniteQuery, InfiniteData } from '@tanstack/react-query';
 import { getMyBookmark } from '@/api/mypages';
 import { MyBookmark } from '@/types/mypages';
 import { MyContentItem } from '@/types/organisms';
+import { useInfiniteScroll } from '@/hooks/api/mypages/useInfinityScroll';
 
 export const useMyBookmarksQuery = () => {
-  const {
-    data: myBookmarks,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-  } = useInfiniteQuery<MyBookmark, Error, MyBookmark, [string], number>({
-    queryKey: ['myBookmarks'],
-    queryFn: ({ pageParam = 0 }: { pageParam?: number }) =>
-      getMyBookmark(pageParam, 20),
-    getNextPageParam: (lastPage) => {
-      return lastPage.last ? undefined : lastPage.number + 1;
-    },
-    initialPageParam: 0,
-    select: (data: InfiniteData<MyBookmark>) => {
-      const firstPage = data.pages[0];
-      return {
-        content: data.pages.flatMap((page) =>
-          page.content.map((item: MyContentItem) => ({
-            ...item,
-            showBookmark: true,
-          })),
-        ),
-        pageable: firstPage.pageable,
-        totalPages: firstPage.totalPages,
-        totalElements: firstPage.totalElements,
-        last: firstPage.last,
-        size: firstPage.size,
-        number: firstPage.number,
-        sort: firstPage.sort,
-        numberOfElements: firstPage.numberOfElements,
-        first: firstPage.first,
-        empty: firstPage.empty,
-      };
-    },
-  });
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteScroll<MyBookmark>(
+      ['myBookmarks'],
+      ({ pageParam = 0 }) => getMyBookmark(pageParam, 20),
+      (data) => {
+        const firstPage = data.pages[0];
+        return {
+          content: data.pages.flatMap((page) =>
+            page.content.map((item: MyContentItem) => ({
+              ...item,
+              showBookmark: true,
+            })),
+          ),
+          pageable: firstPage.pageable,
+          totalPages: firstPage.totalPages,
+          totalElements: firstPage.totalElements,
+          last: firstPage.last,
+          size: firstPage.size,
+          number: firstPage.number,
+          sort: firstPage.sort,
+          numberOfElements: firstPage.numberOfElements,
+          first: firstPage.first,
+          empty: firstPage.empty,
+        };
+      },
+    );
 
-  return { myBookmarks, fetchNextPage, hasNextPage, isFetchingNextPage };
+  return { myBookmarks: data, fetchNextPage, hasNextPage, isFetchingNextPage };
 };
