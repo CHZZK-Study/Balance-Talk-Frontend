00c75d182457248196ddb9f9690be76e00a62554
onejuneee <onejuneee721@gmail.com>
2024-09-03 00:23:37 +0900

    refactor: 페이지네이션 로직 수정

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,3 +1,11 @@
-export const createRangeArray = (length: number) => {
-  return Array.from({ length }, (_, i) => i + 1);
+export const createRangeArray = (currentPage: number, maxPage: number) => {
+  const pagesPerGroup = 5;
+  const currentGroup = Math.floor((currentPage - 1) / pagesPerGroup);
+  const startPage = currentGroup * pagesPerGroup + 1;
+  const endPage = Math.min(startPage + pagesPerGroup - 1, maxPage);
+
+  return Array.from(
+    { length: endPage - startPage + 1 },
+    (_, i) => startPage + i,
+  );
 };
