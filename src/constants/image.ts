cc866a8bda44961763f6a655d0065175e4873dfa
hsgh085 <hsyu0917@gmail.com>
2024-09-14 02:37:19 +0900

    feat: 기본 이미지 서버 경로 상수화

---
+++ src/constants/image.ts
@@ -0,0 +1,32 @@
+export const DEFAULT_PROFILE_URL = {
+  OCTOPUS: {
+    CLIENT: '/octopus-profile.png',
+    SERVER:
+      'https://picko-image.s3.ap-northeast-2.amazonaws.com/member/2c132b00-e37a-4f71-bbf2-e064bd181915_octopus_profile.jpg',
+  },
+  JELLYFISH: {
+    CLIENT: '/jellyfish-profile.png',
+    SERVER:
+      'https://picko-image.s3.ap-northeast-2.amazonaws.com/member/8fcc5073-d977-4b91-a4d1-e242e3eb2efa_jellyfilsh_profile.jpg',
+  },
+  RAY: {
+    CLIENT: '/ray-profile.png',
+    SERVER:
+      'https://picko-image.s3.ap-northeast-2.amazonaws.com/member/0c6c471d-420a-4632-ae9b-20be5a52097c_ray_profile.jpg',
+  },
+  EEL: {
+    CLIENT: '/eel-profile.png',
+    SERVER:
+      'https://picko-image.s3.ap-northeast-2.amazonaws.com/member/873fad2f-1cf5-4429-9cd0-626656b894d3_eel_profile.jpg',
+  },
+  TURTLE: {
+    CLIENT: '/turtle-profile.png',
+    SERVER:
+      'https://picko-image.s3.ap-northeast-2.amazonaws.com/member/4239508d-f9b3-4a3e-91be-3777024ea05a_turtle_profile.jpg',
+  },
+  RABBIT: {
+    CLIENT: '/rabbit-profile.png',
+    SERVER:
+      'https://picko-image.s3.ap-northeast-2.amazonaws.com/member/68f736cf-0f65-4f12-bb44-4f54c448eddc_rabbit_profile.jpg',
+  },
+};
