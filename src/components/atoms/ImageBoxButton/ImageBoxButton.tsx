aae04334cfbf604bcc912ac54e4bbd6c1839967a
onejuneee <onejuneee721@gmail.com>
2024-09-16 21:27:11 +0900

    refactor: ImageBoxButton 컴포넌트 태그 구조 변경

--- src/components/atoms/ImageBoxButton/ImageBoxButton.tsx
+++ src/components/atoms/ImageBoxButton/ImageBoxButton.tsx
@@ -1,50 +1,32 @@
-import React, { useState, useRef } from 'react';
+import React, { ChangeEvent } from 'react';
 import { Camera } from '@/assets';
-import { FileUploadType } from '@/types/file';
-import { useFileUploadMutation } from '@/hooks/api/file/useFileUploadMutation';
 import * as S from './ImageBoxButton.style';
 
-const ImageBoxButton = () => {
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
-  const { mutate } = useFileUploadMutation();
-  const inputRef = useRef<HTMLInputElement | null>(null);
+interface ImageBoxButtonProps {
+  imageFile: File | null;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
 
-  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      const formData = new FormData();
-      formData.append('file', file);
-
-      mutate(
-        { formData, params: { type: 'GAME' } as FileUploadType },
-        {
-          onSuccess: (response) => {
-            if (response.imgUrls && response.imgUrls.length > 0) {
-              setImageUrl(response.imgUrls[0]);
-            }
-          },
-          onError: (error) => {
-            console.error('이미지 업로드 중 오류 발생:', error);
-          },
-        },
-      );
-    }
-  };
+const ImageBoxButton = ({ imageFile, onChange }: ImageBoxButtonProps) => {
+  const fileInputRef = React.useRef<HTMLInputElement | null>(null);
 
   return (
     <div css={S.imageContainer}>
-      {imageUrl ? (
-        <img src={imageUrl} alt="Uploaded" css={S.uploadedImage} />
+      {imageFile ? (
+        <img
+          src={URL.createObjectURL(imageFile)}
+          alt="Uploaded"
+          css={S.uploadedImage}
+        />
       ) : (
-        <label css={S.defaultImageBox} htmlFor="file-upload">
+        <label htmlFor="file-upload" css={S.defaultImageBox}>
           <Camera css={S.iconStyle} />
-          {}
           <input
             id="file-upload"
             type="file"
             accept="image/*"
-            ref={inputRef}
-            onChange={handleImageUpload}
+            ref={fileInputRef}
+            onChange={onChange}
             css={S.fileInput}
           />
         </label>
