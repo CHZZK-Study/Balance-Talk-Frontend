c831623b691ad7304aae223c28ce65b35767075f
areumH <94692566+areumH@users.noreply.github.com>
2024-10-13 21:29:35 +0900

    refactor: OptionInputBox 컴포넌트 스타일 수정 및 스토리북 수정

--- src/components/atoms/OptionInputBox/OptionInputBox.style.ts
+++ src/components/atoms/OptionInputBox/OptionInputBox.style.ts
@@ -2,6 +2,12 @@ import { css } from '@emotion/react';
 import color from '@/styles/color';
 import typo from '@/styles/typo';
 
+export const optionInputStyling = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-end',
+});
+
 export const contentWrapStyle = (hasText: boolean, option: 'A' | 'B') => {
   let outline = `1px solid ${color.GY[2]}`;
   let boxShadowColor = 'rgba(0, 0, 0, 0.05)';
@@ -9,21 +15,15 @@ export const contentWrapStyle = (hasText: boolean, option: 'A' | 'B') => {
   if (hasText) {
     if (option === 'A') {
       outline = `1px solid ${color.RED}`;
-    } else if (option === 'B') {
-      outline = `1px solid ${color.BLUE}`;
-    }
-  }
-
-  if (hasText) {
-    if (option === 'A') {
       boxShadowColor = 'rgba(0, 0, 0, 0.05)';
     } else if (option === 'B') {
+      outline = `1px solid ${color.BLUE}`;
       boxShadowColor = 'rgba(0, 0, 0, 0.05)';
     }
   }
 
   return css(typo.SubTitle, {
-    padding: '30.5px 29px 32.5px 63px',
+    padding: '30.5px 22px 32.5px 63px',
     backgroundColor: color.WT,
     display: 'flex',
     flexDirection: 'row',
@@ -36,6 +36,14 @@ export const contentWrapStyle = (hasText: boolean, option: 'A' | 'B') => {
   });
 };
 
+export const getFocusStyling = css({
+  outline: `1px solid ${color.MAIN}`,
+});
+
+export const getErrorStyling = css({
+  outline: `3px solid ${color.RED}`,
+});
+
 export const optionStyle = (option: 'A' | 'B') =>
   css({
     color: option === 'A' ? color.RED : color.BLUE,
@@ -43,7 +51,8 @@ export const optionStyle = (option: 'A' | 'B') =>
   });
 
 export const textStyle = css({
-  width: '373px',
+  width: '340px',
+  marginRight: '20px',
   color: color.BK,
   '::placeholder': {
     color: color.GY[1],
--- src/stories/atoms/OptionInputBox.stories.tsx
+++ src/stories/atoms/OptionInputBox.stories.tsx
@@ -1,11 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import store from '@/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactQueryProvider from '@/providers/ReactQueryProvider';
 import type { Meta, StoryObj } from '@storybook/react';
 import OptionInputBox from '@/components/atoms/OptionInputBox/OptionInputBox';
+import { storyContainer, storyInnerContainer } from '@/stories/story.styles';
 
 const meta = {
   title: 'atoms/OptionInputBox',
@@ -20,7 +20,7 @@ const meta = {
   args: {
     option: 'A',
     value: '',
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => {},
+    onChange: () => {},
   },
   decorators: [
     (Story) => (
@@ -38,10 +38,34 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    option: 'A',
-    value: '',
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => {},
+export const Default: Story = {};
+
+export const All: Story = {
+  render: () => {
+    const [optionA, setOptionA] = useState<string>('');
+    const [optionB, setOptionB] = useState<string>('');
+
+    return (
+      <ul css={storyContainer}>
+        <li css={storyInnerContainer}>
+          <h3>Option A</h3>
+          <OptionInputBox
+            option="A"
+            value={optionA}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setOptionA(e.target.value)
+            }
+          />
+          <h3>Option B</h3>
+          <OptionInputBox
+            option="B"
+            value={optionB}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setOptionB(e.target.value)
+            }
+          />
+        </li>
+      </ul>
+    );
   },
 };
